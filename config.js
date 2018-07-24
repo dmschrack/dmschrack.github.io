fb08da642017036aecaa8f03268daefc936f99c4
Nathan Harris <nathan.harris@dish.com>
2018-07-24 14:08:40 -0600

    chore(repo): Add new resume

---
+++ config.js
@@ -0,0 +1,171 @@
+var config = {
+  resumeImageSrc: "https://avatars3.githubusercontent.com/u/4176190?v=3&s=460",
+  resumeOwner: "Nate Harris",
+  occupation: "Full-stack Web Developer",
+  location: "Colorado Springs, CO",
+  education: {
+    schoolLogo: "http://gdurl.com/gQTo",
+    schoolName: "Cedarville University",
+    fieldOfStudyInfo: "Majored in Computer Science and Minored in Mathematics and Graphic Design. Graduated with Honor."
+  },
+  moreInfo: [
+    {
+      icon: "fa-github",
+      link: "https://github.com/n8harris",
+      linkName: "github.com/n8harris"
+    },
+    {
+      icon: "fa-gitlab",
+      link: "https://gitlab.com/n8harris",
+      linkName: "gitlab.com/n8harris"
+    },
+    {
+      icon: "fa-linkedin",
+      link: "https://linkedin.com/in/n8harris",
+      linkName: "linkedin.com/in/n8harris"
+    },
+    {
+      icon: "fa-envelope",
+      link: "mailto:nate@natepharris.me",
+      linkName: "nate@natepharris.me"
+    }
+  ],
+  experiences: [
+    {
+      companyName: "SlingTV",
+      companyImage: "http://goo.gl/Qwqey4",
+      position: "Senior Software Developer",
+      timeAtCompany: "May 2017 - present",
+      techUsed: [
+        "ReactJS",
+        "Redux",
+        "Angular2",
+        "Rails",
+        "Elixir"
+      ],
+      accomplishments: [
+        "Currently working on the user management/payment processing system on sling.com",
+        "Currently architecting generic solutions to support a wide range of business/marketing flows",
+        "Currently working on our new platform which will be based on Elixir microservices and a React component library",
+        "Team lead and part of group making architectural decisions for an app that reaches millions of people every day"
+      ]
+    },
+    {
+      companyName: "TipJar",
+      companyImage: "http://gdurl.com/LRez",
+      position: "Co-Founder/Software Developer",
+      timeAtCompany: "January 2017 - present",
+      techUsed: [
+        "Serverless",
+        "AWS",
+        "Lambda",
+        "DynamoDB",
+        "ReactJS"
+      ],
+      accomplishments: [
+        "Currently developing a microservice architecture using the Serverless Framework to support authentication, giving tips, etc.",
+        "Currently working on a beacon service for the apps that will allow customer service employees to automatically send 'review' notifications to anyone they serve who has the app",
+        "Currently working on setting up server provisioning (Chef) for our CI server"
+      ]
+    },
+    {
+      companyName: "Hively",
+      companyImage: "http://gdurl.com/RKvi",
+      position: "Co-Founder/Software Developer",
+      timeAtCompany: "April 2016 - present",
+      techUsed: [
+        "MongoDB",
+        "CakePHP",
+        "BackboneJS",
+        "Gulp",
+        "Jenkins",
+        "PHPUnit",
+        "Karma"
+      ],
+      accomplishments: [
+        "Sole developer on project",
+        "Created a Single Page Application using CakePHP on the backend and BackboneJS for the frontend",
+        "Created REST API for use with web application as well as mobile apps",
+        "Created system to match employers with potential employees based on personality and company culture and used MapReduce algorithm to improve matching performance"
+      ]
+    },
+    {
+      companyName: "Focus on the Family",
+      companyImage: "http://gdurl.com/eYg6",
+      position: "Software Developer",
+      timeAtCompany: "May 2015 - present",
+      techUsed: [
+        "MySQL",
+        "NodeJS",
+        "AngularJS",
+        "Ruby",
+        "Chef",
+        "Java",
+        "Objective-C"
+      ],
+      accomplishments: [
+        "Lead developer on project that reaches millions of people per month",
+        "Improved efficiency by creating recipes using Chef, implementing deployment scripts utilized by Jenkins, and developing Slack bots for the team to use",
+        "Redesigned payment system for product allowing multiple payment plans as well as a free trial option",
+        "Improved release time on products from 2 hours to a couple minutes",
+        "Improved testing by adding end to end tests and visual tests",
+        "Implemented numerous features on our suite of Android mobile applications"
+      ]
+    },
+    {
+      companyName: "Air Force Research Lab",
+      companyImage: "http://gdurl.com/CVAU",
+      position: "Software Engineer",
+      timeAtCompany: "May 2014 - May 2015",
+      techUsed: [
+        "C#",
+        "Python",
+        "VBScript",
+        "C++"
+      ],
+      accomplishments: [
+        "Developed an application using C# and Python to help the senior engineers more easily perform tests",
+        "Responsible for all software development on radar simulator project",
+        "Improved UX by adding data visualization of flight trajectories",
+        "Improved code coverage on tests to 80%"
+      ]
+    },
+    {
+      companyName: "Ball Aerospace",
+      companyImage: "http://gdurl.com/B51P",
+      position: "Software Engineer Intern",
+      timeAtCompany: "May 2013 - May 2014",
+      techUsed: [
+        "PHP",
+        "Memcached",
+        "CSS",
+        "HTML",
+        "JavaScript"
+      ],
+      accomplishments: [
+        "Worked as an intern during the summer",
+        "Developed a web-based application using PHP and Java",
+        "Improved UX of application by doing A/B testing",
+        "Improved database performance by adding indices and eliminating slow queries",
+        "Reduced stress on db by saving some db call results in memory using Memcached"
+      ]
+    },
+    /*{
+      companyName: "Cedarville University",
+      companyImage: "http://gdurl.com/gQTo",
+      position: "Web Developer",
+      timeAtCompany: "February 2012 - March 2014",
+      techUsed: [
+        "ColdFusion",
+        "jQuery",
+        "ASP.NET MVC"
+      ],
+      accomplishments: [
+        "Worked as a web developer during school",
+        "Added features to cedarville.edu as well as internal applications",
+        "Developed features using C#, ColdFusion, and jQuery",
+        "Implemented new mobile friendly design"
+      ]
+    }*/
+  ]
+}
---
+++ controller.js
@@ -0,0 +1,5 @@
+var app = angular.module('resumev2', []);
+
+app.controller('ResumeController', ['$scope', function($scope) {
+    this.config = config;
+}]);
